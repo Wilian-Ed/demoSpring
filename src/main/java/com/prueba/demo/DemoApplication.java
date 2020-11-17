54d9ee2bfad26dfdb158fab4b6156a7acdd97081
Wilian Ed <wilian122397@gmail.com>
2020-11-17 17:59:44 -0600

    17/11 17 59

--- src/main/java/com/prueba/demo/DemoApplication.java
+++ src/main/java/com/prueba/demo/DemoApplication.java
@@ -2,11 +2,17 @@ package com.prueba.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public SpringApplicationContext springApplicationContext() {
+        return new SpringApplicationContext();
     }
 }
